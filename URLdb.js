0901d74cd21b04de30e2ef70c554f052b56695ef
Dave Smith <lah4life@gmail.com>
2017-02-11 01:23:33 +0000

    Remove database connection from URLdb. and add to server.js to reduce number of active connections. Add or clause to dbURL constant to adjust for local and heroku connections.

--- URLdb.js
+++ URLdb.js
@@ -1,34 +1,22 @@
-const mongo = require("mongodb").MongoClient;
-const dbURL = process.env.MONGO_DBURL;
-
-
-function highestShortURL(){
-        return mongo.connect(dbURL).then((db)=>{
-            return db.collection('urls').find({}).sort({'shortURL': -1}).limit(1).next();
-        });
+function highestShortURL(db){
+        return db.collection('urls').find({}).sort({'shortURL': -1}).limit(1).next();
     }
 
 module.exports = {
-    addURL: function(urlData){
-     return mongo.connect(dbURL).then((db)=>{
-         return db.collection('urls').insert(urlData);
-     });   
+    addURL: function(urlData, db){
+     return db.collection('urls').insert(urlData);
     },
     
-    findShortURL: function(url){
-        return mongo.connect(dbURL).then((db)=>{
-            return db.collection('urls').findOne({shortURL: parseInt(url)}, {_id: 0, url: 1});
-        });   
+    findShortURL: function(url, db){
+        return db.collection('urls').findOne({shortURL: parseInt(url)}, {_id: 0, url: 1});
     },
     
-    findURL: function(url){
-        return mongo.connect(dbURL).then((db)=>{
-            return db.collection('urls').findOne({url: url}, {_id: 0});
-        });
+    findURL: function(url, db){
+        return db.collection('urls').findOne({url: url}, {_id: 0});
     },
     
-    generateShortURL: function(){
-       return highestShortURL().then((highest)=>{
+    generateShortURL: function(db){
+       return highestShortURL(db).then((highest)=>{
            var increment = parseInt(highest.shortURL) + 1;
            return increment;    
        }); 
--- server.js
+++ server.js
@@ -1,68 +1,79 @@
-// Define dependencies and envirnment constants
-const db = require("./URLdb");
+// Define dependencies and environment constants
+const api = require("./URLdb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
 const appPath = 'https://aqueous-savannah-48973.herokuapp.com/';
-// 
-//START ROUTES
-// 
-// Route for new shortened url requests
-app.get('/new/*', (req, res)=>{
-    // Get the url from the request
-    var url = req.url.slice(5);
-    // Add http to the requrest if http not found at the start of the url
-    if(url.slice(0,4) !== 'http'){
-        url = "http://" + url;
-    }
-    
-    if(isURL(url)){
-        // If the url already exists return the existing document
-        db.findURL(url).then((foundResult)=>{
-            if(foundResult){
-                foundResult.shortURL = appPath + foundResult.shortURL;
-                return res.send(foundResult, 200);
-            }else{
-                // If the url does not exist add it to the database and
-                // return the document
-                db.generateShortURL().then((shortURL)=>{
-                    var urlData = {url: url, shortURL: shortURL};
-                    db.addURL(urlData).then(function(result){
-                        var receipt =  result.ops[0];
-                        delete receipt._id;
-                        receipt.shortURL = appPath + receipt.shortURL;
-                        return res.send(receipt, 200);
-                    });    
-                });
-                
-            }
-        });
-        
-    }else{
-        // Return an error if not a valid url
-        return res.send({error: 'You did not use a valid url format. Try again.'}, 200);
-    }
-});
+const mongo = require("mongodb").MongoClient;
+const dbURL = process.env.MONGO_DBURL || 'mongodb://localhost:27017/URLdb';
 
-//Route for shortened url requests
-app.get('/*', (req, res)=>{
-    // Get the url from the request
-    var url = req.url.slice(1);
-    // If no url provided show main page message
-    if(url == ''){
-        return res.sendFile(path.join(__dirname + '/home.html'));
+mongo.connect(dbURL, (err, db)=>{
+    // Report error or success connecting to database
+    if(err){
+        throw new Error('Failed to connect to database :(');
     }else{
-    //If url is provided and short url is in the database redirect
-    //  to the full url otherwise retrun an error
-        db.findShortURL(url).then((foundResult)=>{
-           if(foundResult){
-               return res.redirect(foundResult.url);
-           }else{
-               return res.send({error: 'The url :' + url + ': provided is not in the database'}, 200);
-           }
-        });
+        console.log('Connected to database on port 27017 :)');
     }
+    // 
+    //START ROUTES
+    // 
+    // Route for new shortened url requests
+    app.get('/new/*', (req, res)=>{
+        // Get the url from the request
+        var url = req.url.slice(5);
+        // Add http to the request if http not found at the start of the url
+        if(url.slice(0,4) !== 'http'){
+            url = "http://" + url;
+        }
+        
+        if(isURL(url)){
+            // If the url already exists return the existing document
+            api.findURL(url, db).then((foundResult)=>{
+                if(foundResult){
+                    foundResult.shortURL = appPath + foundResult.shortURL;
+                    return res.status(200).send(foundResult);
+                }else{
+                    // If the url does not exist add it to the database and
+                    // return the document
+                    api.generateShortURL(db).then((shortURL)=>{
+                        var urlData = {url: url, shortURL: shortURL};
+                        api.addURL(urlData, db).then(function(result){
+                            var receipt =  result.ops[0];
+                            delete receipt._id;
+                            receipt.shortURL = appPath + receipt.shortURL;
+                            return res.status(200).send(receipt);
+                        });    
+                    });
+                    
+                }
+            });
+            
+        }else{
+            // Return an error if not a valid url
+            return res.status(200).send({error: 'You did not use a valid url format. Try again.'});
+        }
+    });
+    
+    //Route for shortened url requests
+    app.get('/*', (req, res)=>{
+        // Get the url from the request
+        var url = req.url.slice(1);
+        // If no url provided show homepage
+        if(url == ''){
+            return res.sendFile(path.join(__dirname + '/home.html'));
+        }else{
+        //If url is provided and short url is in the database redirect
+        //  to the full url otherwise retrun an error
+            api.findShortURL(url, db).then((foundResult)=>{
+               if(foundResult){
+                   return res.redirect(foundResult.url);
+               }else{
+                   return res.status(200).send({error: 'The url :' + url + ': provided is not in the database'});
+               }
+            });
+        }
+    });
 });
 /*START SERVER*/
 app.listen(port, ()=>{
